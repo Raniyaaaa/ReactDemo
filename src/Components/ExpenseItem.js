cf0a59252c4ad67e7129a0bdaf48c3a831cab9c2
Raniyaaaa <raniya182002@gmail.com>
2024-10-15 23:21:54 +0530

    using multiple div

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,10 +1,13 @@
 function ExpenseItem() {
     return(
+      <>
+        <div>15 August 2023</div>
         <div>
-            <p>Food - $5</p>
-            <p>Petrol - $20</p>
-            <p>Movie - $10</p>
+          <h1>Book</h1>
+          <div>$10</div>
         </div>
+        <div>Delhi</div>
+      </>
     )
 }
 export default ExpenseItem;
