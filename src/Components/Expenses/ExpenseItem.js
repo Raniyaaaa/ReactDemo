bc31806541f334b2bcd4f00ae0e5a709310daffb
Raniyaaaa <raniya182002@gmail.com>
2024-10-17 16:16:17 +0530

    Event Handler added

---
+++ src/Components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card"
+
+const ExpenseItem =(props)=>{
+  function buttonClickHandler(event){
+    console.log("Button Clicked");
+  }
+  return (
+    <Card className="expense-item">      
+    <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
+      </div>
+      <button onClick={buttonClickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
