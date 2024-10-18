ee416f0deafd757166a3b8b7e13b808140198379
Raniyaaaa <raniya182002@gmail.com>
2024-10-18 22:25:14 +0530

    created new component for the expenselist

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React,{useState} from "react";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css"
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter"
+import ExpenseList from "./ExpenseList";
 const Expenses = (props)=>{
 
   const [filteredYear, setFilterYear]= useState("2023");
@@ -10,31 +10,13 @@ const Expenses = (props)=>{
         setFilterYear(selectedYear);
   };
   const filteredExpenses=props.expenses.filter(expense=>expense.date.getFullYear().toString()===filteredYear);
-
-  let expensesContent = <p>No expenses found</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => {
-      return (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          date={expense.date}
-          price={expense.price}
-        />
-      );
-    });
-  }
-
-  
     return(
        <Card className="expenses">
         <ExpenseFilter 
           selected={filteredYear} 
           onChangeFilter={changeFilterHandler}
         />
-        {expensesContent}
-        {filteredExpenses.length===1 && <p>Only one expense here. Please add more</p>}
+        <ExpenseList items={filteredExpenses}/>
        </Card>
     )
   }
